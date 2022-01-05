648420997e00b2c66af2db7c591b6c5cd356a9de
mohit-kumar-behera <mohitkumar.b3399@gmail.com>
2022-01-05 23:31:13 +0530

    add param

--- news/views.py
+++ news/views.py
@@ -1,15 +1,16 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse,JsonResponse,HttpResponseRedirect
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.utils import timezone
 from django.urls import reverse
 import datetime
 import requests
 
-def home(request,countryCode):
-	url = f'http://newsapi.org/v2/top-headlines?country={countryCode}&pageSize=100&from={timezone.now()-datetime.timedelta(days=7)}&sortBy="popularity"&apiKey=56168070f1c9446cb98dade52f70a719'
+def home(request, countryCode):
+	ENDPOINT_URL = 'http://newsapi.org/v2/top-headlines'
+	time_from = timezone.now() - datetime.timedelta(days=7)
+	url = f'{ENDPOINT_URL}?country={countryCode}&pageSize=100&from={time_from}&sortBy="popularity"&apiKey=56168070f1c9446cb98dade52f70a719'
 	response = requests.get(url)
-	print("****************************")
-	print(response.status_code)
+
 	if response.status_code == 200:
 		context = dict(response.json())
 	else:
